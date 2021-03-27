af70866769bcc65c72d576ea416dd9ca8a03987d
disha00991 <directionstar86@gmail.com>
2021-03-27 15:24:14 -0400

    q:9-q:10 done

--- code/TreeClassifier.py
+++ code/TreeClassifier.py
@@ -1,4 +1,5 @@
 import numpy as np
+import csv
 from sklearn.model_selection import KFold
 from sklearn.tree import DecisionTreeClassifier
 
@@ -13,12 +14,12 @@ def question_4(X_trn, y_trn, X_tst):
     for depth in max_depth:
 
         fold_scores = []
-        classifier = ''
+        classifier = DecisionTreeClassifier(random_state=1, max_depth=depth)
         for train_index, test_index in kf.split(X_trn):
             Xk_train, Xk_test = X_trn[train_index], X_trn[test_index]
             yk_train, yk_test = y_trn[train_index], y_trn[test_index]
 
-            classifier = train_tree_classifier(Xk_train, yk_train, depth)
+            classifier.fit(Xk_train, yk_train)
             fold_scores.append(classifier.score(Xk_test, yk_test))
         
         error = 1 - np.mean(np.array(fold_scores))
@@ -26,10 +27,34 @@ def question_4(X_trn, y_trn, X_tst):
         depth_errors.append(error)
 
     best_depth = max_depth[np.argmin(np.array(depth_errors))]
-    print(f"best depth : {best_depth}")         
+    print(f"best depth : {best_depth}") 
 
-def train_tree_classifier(X_trn, y_trn, max_depth):
-    classifier = DecisionTreeClassifier(random_state=1, max_depth=max_depth)
+def question_5(X_trn, y_trn, X_tst):
+    #get k folds (k=5)
+    kf = KFold(n_splits=5)
+    print("no of splits: ", kf.get_n_splits(X_trn))
+
+    fold_scores = []
+    classifier = DecisionTreeClassifier(random_state=1, max_depth=6) ##best depth = 6 returned by question 4
+    for train_index, test_index in kf.split(X_trn):
+        Xk_train, Xk_test = X_trn[train_index], X_trn[test_index]
+        yk_train, yk_test = y_trn[train_index], y_trn[test_index]
+
+        classifier.fit(Xk_train, yk_train)
+        fold_scores.append(classifier.score(Xk_test, yk_test))        
+    
+    print("Estimated generalization error: ", 1 - np.mean(np.array(fold_scores)))
     classifier.fit(X_trn, y_trn)
-    return classifier
+
+    predictions = classifier.predict(X_tst)
+    write_csv(predictions, 'kfold.csv')
+
+def write_csv(y_pred, filename):
+    """Write a 1d numpy array to a Kaggle-compatible .csv file"""
+    with open(filename, 'w') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['Id', 'Category'])
+        for idx, y in enumerate(y_pred):
+            csv_writer.writerow([idx, y])      
+
 
--- code/run_me.py
+++ code/run_me.py
@@ -1,7 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-from TreeClassifier import question_4
+from TreeClassifier import question_4, question_5
+from LinearModels import question_9, question_10
 
 stuff=np.load("../report_src/data.npz")
 X_trn = stuff["X_trn"]
@@ -18,7 +19,21 @@ def show(x):
 ####Q:4 Train 6 different classification trees on the image data, with each of the following maximum depths
 ####{1,3,6,9,12,14}
 ####use 5-fold cross validation
-question_4(X_trn, y_trn, X_tst)
+# question_4(X_trn, y_trn, X_tst)
+
+####Q:5 Use best depth in above question to predict results for test data and uplaod to kaggle
+####best depth above is 6
+# question_5(X_trn, y_trn, X_tst)
+
+####Q:9 For both hinge loss and logistic loss, train linear models with ridge regularization
+####train the model and estimate the mean out of sample loss/error using 5-fold cross-validation
+####l = {.0001,.001,1,10,100}
+question_9(X_trn, y_trn, X_tst)
+
+#### Q:10 Choose the training loss and \lambda that you think will perform best
+#### Make predictions for the test data
+####best lambda above: , best training loss estimator: 
+# question_10(X_trn, y_trn, X_tst)
 
 
 
